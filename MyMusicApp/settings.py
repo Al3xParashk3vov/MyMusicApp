c42204cbd9a6d5bc8e811e0c2ca8af9017d48dee
Alexander Parashkevov <122312028+Al3xParashk3vov@users.noreply.github.com>
2024-10-24 14:53:30 +0300

    Did all of the stuff. The app is pretty much done

--- MyMusicApp/settings.py
+++ MyMusicApp/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'albums.apps.AlbumConfig',
+    'profiles.apps.ProfilesConfig',
+    'common.apps.CommonConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,9 +78,13 @@ WSGI_APPLICATION = 'MyMusicApp.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "exam_prep_music_app_softuni",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
@@ -117,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
---
+++ profiles/validators.py
@@ -0,0 +1,27 @@
+from xml.dom import ValidationErr
+
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+from django.utils.text import slugify
+
+
+@deconstructible
+class AlphaNumericValidator:
+    def __init__(self, message=None):
+        self.message = message
+
+    @property
+    def message(self):
+        return self.__message
+
+    @message.setter
+    def message(self, value):
+        if value is None:
+            self.__message = "Ensure this value contains only letters, numbers, and underscore."
+        else:
+            self.__message = value
+    # can do without these ^
+
+    def __call__(self, value:str, *args, **kwargs):
+        if '-' in value or value.lower() != slugify(value): # Dido slugify(Dido) -> dido
+            raise ValidationError(self.message)
